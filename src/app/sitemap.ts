cbae1cb3ce71a21fbe2ec14da1901c723553f047
stuartfreaks <stuartfreaks@me.com>
2026-05-27 12:38:18 +0200

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://ourmaninmilan.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/scribblings',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/shop',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/scribblings/milan-the-new-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/scribblings/dal-vinattiere',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/scribblings/frangente-friday-night',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://ourmaninmilan.com/scribblings/kiwon-kimchi-revolution',
+      lastModified: new Date(),
+    },
+  ];
+}
